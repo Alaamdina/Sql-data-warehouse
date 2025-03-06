14d536a0c031ff01b5fd9baa5be52a01749897c8
Alaamdina <134615924+Alaamdina@users.noreply.github.com>
2025-03-06 11:25:00 +0200

    Create database.sql

---
+++ scripts/database.sql
@@ -0,0 +1,14 @@
+/*
+Here we will create a database called AlaaDatawarehouse then we will create three schemas within the database 
+*/
+create database AlaaDatawarehouse;
+GO
+
+USE AlaaDatawarehouse;
+GO
+
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
